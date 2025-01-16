cf96ee960d8c4f2a9bcbc3475009942a3c1a1929
unknown <willyyumbum@yahoo.com>
2025-01-16 13:37:43 -0500

    bearer auth scheme

--- controllers/login.js
+++ controllers/login.js
@@ -22,7 +22,14 @@ loginRouter.post('/', async (request, response) => {
         id: user._id,
     };
 
-    const token = jwt.sign(userForToken, process.env.SECRET);
+    // token expires in 60*60 seconds, that is, in one hour
+    const token = jwt.sign(
+        userForToken,
+        process.env.SECRET,
+        { expiresIn: 60*60 }
+    );
+
+    // const token = jwt.sign(userForToken, process.env.SECRET);
 
     response
         .status(200)
